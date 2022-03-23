298ebcb030e42f9ef09eab0f575205772fc9761c
flipperflipper <caylaphil@gmail.com>
2022-03-23 23:12:34 +0100

    correction of bugs concerning card content and background images not properly applying on reverse class

--- src/components/BlockContent/index.js
+++ src/components/BlockContent/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import PropTypes from 'prop-types';
 import { style } from 'glamor';
 import './style.scss';
@@ -5,8 +6,21 @@ import './style.scss';
 export default function BlockLeft({
   id, image, imageHover, subtitle, title, text, link,
 }) {
-  const styles = {
+  const styles1 = {
     left__block__img: style({
+      backgroundImage: `url(${image})`,
+      backgroundPosition: 'center left',
+      backgroundSize: 'cover',
+      width: '50%',
+      transition: '0.2s ease-out',
+      ':hover': {
+        backgroundImage: `url(${imageHover})`,
+        transition: '0.2s ease-in',
+      },
+    }),
+  };
+  const styles2 = {
+    left__block__reverse__img: style({
       backgroundImage: `url(${image})`,
       backgroundPosition: 'center left',
       backgroundSize: 'cover',
@@ -28,7 +42,7 @@ export default function BlockLeft({
 
   return (
     <div className={isEven(id) ? 'left__block__reverse' : 'left__block'}>
-      <section className={styles.left__block__img} />
+      <section className={isEven(id) ? styles2.left__block__reverse__img : styles1.left__block__img} />
       <section className="left__block__block">
         <span className="left__block__block__sub">{subtitle}</span>
         <a className="left__block__block__title">{title}</a>
@@ -36,7 +50,7 @@ export default function BlockLeft({
         <p className="left__block__block__text">
           {text}
         </p>
-        <a>{link}</a>
+        <a className="left__block__block__link">{link}</a>
       </section>
     </div>
   );
